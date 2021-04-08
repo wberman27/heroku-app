2f25322b5d4b4890ce79f7e3135131c5573988f6
Will B <wberman27@gmail.com>
2021-04-08 12:59:40 -0400

    a comment

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const server = express()
 server.use(cors())
 server.use(express.json()) //for body requests, so not always needed
 
-const port = process.env.PORT || 9000 //default to 9000 if no port found
+const port = process.env.PORT || 9000 //use heroku's port, default to 9000 if no port found
 
 server.listen(port, ()=>{
     console.log(`Listening on Port ${port}`)
